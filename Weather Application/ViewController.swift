52625f2828f5ef07bf665570801f52eb6a2e242c
Mert Safaktepe <safaktepemert@gmail.com>
2022-05-15 14:37:28 +0300

    Initial Commit

---
+++ Weather Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather Application
+//
+//  Created by Mert Şafaktepe on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
